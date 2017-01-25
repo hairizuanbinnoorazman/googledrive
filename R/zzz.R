b13f71b1aaac16444baf519d86ce94ca88e381a9
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-01-25 08:56:03 +0800

    chore: Added API endpoints to package

--- R/zzz.R
+++ R/zzz.R
@@ -5,12 +5,21 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
   op.slides <- list(
-    slides.client.id = "10709400262-28lpv43nui21l1172cup6kh68blvkllq.apps.googleusercontent.com",
-    slides.client.secret = "FADkDoE_H0B7j-VytEjTbgaU",
-    slides.endpoint.create = "https://slides.googleapis.com/v1/presentations",
-    slides.endpoint.get = "https://slides.googleapis.com/v1/presentations/{presentationId}",
-    slides.endpoint.batchUpdate = "https://slides.googleapis.com/v1/presentations/{presentationId}:batchUpdate",
-    slides.endpoint.page.get = "https://slides.googleapis.com/v1/presentations/{presentationId}/pages/{pageObjectId}"
+    drive.client.id = "10709400262-28lpv43nui21l1172cup6kh68blvkllq.apps.googleusercontent.com",
+    drive.client.secret = "FADkDoE_H0B7j-VytEjTbgaU",
+
+    # Files Resource Representation
+    drive.endpoint.files.copy = "https://www.googleapis.com/drive/v3/files/{fileId}/copy",
+    drive.endpoint.upload.files.create = "https://www.googleapis.com/upload/drive/v3/files",
+    drive.endpoint.files.generateIds = "https://www.googleapis.com/drive/v3/files/generateIds",
+    drive.endpoint.files.get = "https://www.googleapis.com/drive/v3/files/{fileId}",
+    drive.endpoint.files.list = "https://www.googleapis.com/drive/v3/files",
+
+    # Permissions Resource Representation
+    drive.endpoint.permissions.create = "https://www.googleapis.com/drive/v3/files/{fileId}/permissions",
+    drive.endpoint.permissions.delete = "https://www.googleapis.com/drive/v3/files/{fileId}/permissions/{permissionId}",
+    drive.endpoint.permissions.get = "https://www.googleapis.com/drive/v3/files/{fileId}/permissions/{permissionId}",
+    drive.endpoint.permissions.list = "https://www.googleapis.com/drive/v3/files/{fileId}/permissions"
   )
   toset <- !(names(op.slides) %in% names(op))
   if (any(toset)) options(op.slides[toset])
