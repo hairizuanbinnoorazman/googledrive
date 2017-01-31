86a8247bd52defd3df92ee07505d37c5c4dab2a3
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-01-31 23:39:31 +0800

    feat: Created function to list folders within folders

--- R/files.R
+++ R/files.R
@@ -1,8 +1,24 @@
 #' Get the list of files from Googledrive
+#' @description Utilizes the list_files Google Drive API endpoint to retrieves files and folder details.
+#' This function provides the raw functionality of full access to the list_files API endpoint but is not
+#' the recommended way of accessing the API. It would be better to use the higher level functions provided
+#' within this package to access the files/folders/data that you need.
+#' @param q Optional. A query for filtering the file results. See the "Search for Files" guide for supported syntax.
+#' @param pageSize Optional. The maximum number of files to return per page. Acceptable values are 1 to 1000,
+#' inclusive. (Default: 100)
+#' @param pageToken Optional. The token for continuing a previous list request on the next page. This should be
+#' set to the value of 'nextPageToken' from the previous response.
+#' @param orderBy Optional. A comma-separated list of sort keys. Valid keys are 'createdTime', 'folder',
+#' 'modifiedByMeTime', 'modifiedTime', 'name', 'quotaBytesUsed', 'recency', 'sharedWithMeTime',
+#' 'starred', and 'viewedByMeTime'. Each key sorts ascending by default, but may be reversed with the
+#' 'desc' modifier. Example usage: ?orderBy=folder,modifiedTime desc,name.
+#' @param spaces Optional. A comma-separated list of spaces to query within the corpus. Supported
+#' values are 'drive', 'appDataFolder' and 'photos'.
+#' @param corpus Optional. The source of files to list. Acceptable values are domain and user
 #' @importFrom httr config accept_json content
 #' @importFrom jsonlite fromJSON
 #' @export
-list_files <- function(q = NULL, pageSize = NULL, pageToken = NULL,
+base_list_files <- function(q = NULL, pageSize = NULL, pageToken = NULL,
                        orderBy = NULL, spaces = NULL, corpus = NULL){
   # Get endpoint url
   url <- get_endpoint("drive.endpoint.files.list")
@@ -28,3 +44,27 @@ list_files <- function(q = NULL, pageSize = NULL, pageToken = NULL,
   }
   return(result_list)
 }
+
+#' Get list of files from a folder
+#' @param id ID of the drive folder
+#' @inheritParams base_list_files
+#' @export
+list_files_in_folder <- function(id = NULL, pageSize = NULL, pageToken = NULL,
+                                  orderBy = NULL, spaces = NULL, corpus = NULL){
+  q = paste0("'", id, "' in parents and mimeType != 'application/vnd.google-apps.folder' and trashed = false")
+  output <- base_list_files(q, pageSize, pageToken, orderBy, spaces, corpus)
+  return(output)
+}
+
+#' Get list of folders from a folder
+#' @param id ID of the drive folder
+#' @inheritParams base_list_files
+#' @export
+list_folders_in_folder <- function(id = NULL, pageSize = NULL, pageToken = NULL,
+                                 orderBy = NULL, spaces = NULL, corpus = NULL){
+  q = paste0("'", id, "' in parents and mimeType = 'application/vnd.google-apps.folder' and trashed = false")
+  output <- base_list_files(q, pageSize, pageToken, orderBy, spaces, corpus)
+  return(output)
+}
+
+
